37ec4204632783d2e47b4425a07f34472637f80a
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2020-06-08 01:53:05 -0700

    modified to make static files work

--- TestPro/urls.py
+++ TestPro/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views as v1
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.onLoad),
-    path('login/',views.login)
-]
+    path('',v1.onLoad),
+    path('login/',v1.login)
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+
