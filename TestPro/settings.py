d446e790d5b2c8157807a13aca0cd0f21f2ecdc4
Lokesh Vadlamudi <lokesh.dev96@gmail.com>
2020-06-08 02:36:05 -0700

    update1

--- TestPro/settings.py
+++ TestPro/settings.py
@@ -116,11 +116,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATICFILES_DIRS = [
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
     'C:\\Users\\16692\\Desktop\\TestPro\\static',
-]
+)
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+
 
